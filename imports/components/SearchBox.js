8c911f06c6624656b4768f73f74d781bca082651
Eric Tran <tung.tran@naustud.io>
2017-12-19 18:04:10 +0700

    implement search song

--- imports/components/SearchBox.js
+++ imports/components/SearchBox.js
@@ -65,7 +65,10 @@ class SearchBox extends Component {
 	render() {
 		return (
 			<li className="col col--7 navbar__item navbar__item--input">
-				<form onSubmit={this.onFormSubmit} className={`search-box ${this.state.focusSearchBox ? 'search-box--focus' : ''}`}>
+				<form
+					onSubmit={this.onFormSubmit}
+					className={`search-box ${this.state.focusSearchBox ? 'search-box--focus' : ''} ${this.state.searchResult ? 'search-box--active' : ''}`}
+				>
 					<div className="search-box__wrap">
 						<div className="search-box__input">
 							<input
@@ -84,16 +87,16 @@ class SearchBox extends Component {
 							onClick={this.submitSong}
 						/>
 					</div>
+					{this.state.searchResult ? (
+						<div className="search-box__result-wrapper">
+							<ul className="song-result__list">
+								{this.state.searchResult.map((song) => (
+									<li key={song._id} className="song-result__item js-song-result__item {{_active}}" data-href={song.originalURL}>{song.name} • {song.artist} • {song.origin}</li>
+								))}
+							</ul>
+						</div>
+					) : null}
 				</form>
-				{this.state.searchResult ? (
-					<div className="search-box__result-wrapper">
-						<ul className="song-result__list">
-							{this.state.searchResult.map((song) => (
-								<li className="song-result__item js-song-result__item {{_active}}" data-href={song.originalURL}>{song.name} • {song.artist} • {song.origin}</li>
-							))}
-						</ul>
-					</div>
-				) : null}
 			</li>
 		);
 	}
