39f3db747513b4323f011693904e12c1102ed8c9
Thanh Tran <trongthanh@gmail.com>
2017-12-20 19:53:04 +0700

    Update mongodb scripts to current year

--- private/mongodbscripts/get-song-counts.js
+++ private/mongodbscripts/get-song-counts.js
@@ -1,2 +1,10 @@
-// db.getCollection('songs').find({}).count()
-db.getCollection('songs').find({timeAdded: { $lt: (new Date("2016-12-31T23:59:59Z")).getTime(), $gt: (new Date("2016-01-01T00:00:00Z")).getTime()} }).count()
+/* eslint-disable */
+db
+	.getCollection('songs')
+	.find({
+		timeAdded: {
+			$lt: new Date('2017-12-31T23:59:59Z').getTime(),
+			$gt: new Date('2017-01-01T00:00:00Z').getTime(),
+		},
+	})
+	.count();
